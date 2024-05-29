b028a615c5f760c2de2db47d708b62137c601f08
Muhammed Suhail T <19mcs04@meaec.edu.in>
2024-05-29 14:27:57 +0530

    Calculator First

---
+++ Calculator.js
@@ -0,0 +1,12 @@
+function DisplayData(n){
+    result.value+=n
+}
+function findout(){
+    result.value=eval(result.value)
+}
+function Allclear(){
+    result.value=""
+}
+function BackSpacing(){
+    result.value=result.value.slice(0,-1)
+}
